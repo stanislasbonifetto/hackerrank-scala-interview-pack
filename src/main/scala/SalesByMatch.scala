10f58b434b616bbad41cfc0366b132bfd3e49f2e
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2021-07-27 21:07:04 +0100

    solve Sales by Match

---
+++ src/main/scala/SalesByMatch.scala
@@ -0,0 +1,46 @@
+import java.io.PrintWriter
+import scala.io.StdIn
+
+object Result {
+
+  /*
+   * Complete the 'sockMerchant' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts following parameters:
+   *  1. INTEGER n
+   *  2. INTEGER_ARRAY ar
+   */
+
+  def sockMerchant(n: Int, ar: Array[Int]): Int = {
+    // Write your code here
+    val pile: Pile = ar.foldLeft[Pile](Pile()) { (p, sock) => {
+      if (p.spares.contains(sock)) {
+        val pairCount = p.pairs.getOrElse(sock, 0) + 1
+        val spares = p.spares.-(sock)
+        val pairs = p.pairs.+(sock -> pairCount)
+        Pile(spares, pairs)
+      } else Pile(p.spares.+(sock), p.pairs)
+    }}
+    pile.pairs.foldLeft(0)((n, p) => n + p._2)
+  }
+  case class Pile(spares : Set[Color] = Set[Color](), pairs : Map[Color, Int] = Map[Color, Int]())
+
+  type Color = Int
+}
+
+object Solution {
+  def main(args: Array[String]) {
+    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+    val n = StdIn.readLine.trim.toInt
+
+    val ar = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+    val result = Result.sockMerchant(n, ar)
+
+    printWriter.println(result)
+
+    printWriter.close()
+  }
+}
---
+++ src/test/scala/SalesByMatchTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.{FunSuite, Matchers}
+
+class SalesByMatchTest extends FunSuite with Matchers {
+
+  test("sockMerchant 10 20 20 10 10 30 50 10 20 should be 3") {
+    Result.sockMerchant(9, Array(10,20,20,10,10,30,50,10,20)) should be (3)
+  }
+}
